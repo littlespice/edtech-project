1b6d46016ef4bb35ed03e7c786df62bbdbee6581
Jeremy Ju <jeremy.ju@motional.com>
2021-07-18 11:54:25 -0700

    Added type handling for sample 3

--- sample_3.py
+++ sample_3.py
@@ -1,4 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        return self.fib(n-1)+self.fib(n-2) if n>1 else n
-
+        if isinstance(n, int):
+            if n <= 0:
+                return 0
+            elif n == 1:
+                return 1
+            else:
+                return self.fib(n-1)+self.fib(n-2) if n>1 else n
+        else:
+            print('Input n should be of Integer type')
